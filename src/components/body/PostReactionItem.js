513c293551a65059be2ae5ddeec484c73cd0e4f8
Fahad <fhdnaufal@gmail.com>
2024-01-15 12:06:37 +0530

    fixed bugs

--- src/components/body/PostReactionItem.js
+++ src/components/body/PostReactionItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PostReactionItem = ({icon,title}) => {
   return (
-    <div className=' md:text-2xl text-sm font-semibold flex gap-2 text-gray-500 items-center cursor-pointer hover:bg-gray-200 rounded-md  py-3'>
+    <div className=' md:text-2xl text-sm font-semibold flex sm:gap-4 text-gray-500 items-center cursor-pointer hover:bg-gray-200 rounded-md  py-3'>
         <div className='scale-150 max-w-[40px]'>
           {icon}
         </div>
